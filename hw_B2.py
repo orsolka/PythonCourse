8a026ac0cf5ed07171688e54e565758b07511219
orsolka <48160331+orsolka@users.noreply.github.com>
2019-03-15 16:53:30 +0300

    Update hw_B2.py

--- hw_B2.py
+++ hw_B2.py
@@ -1,17 +1,7 @@
-t = str(input(''))
-m = ''
-i = 0
-z = ''
-while i < len(t):
-    x = t[i]
-    if x == 'A':
-        z = 'T'
-    if x == 'T':
-        z = 'A'
-    if x == 'C':
-        z = 'G'
-    if x == 'G':
-        z = 'C'
-    i = i+1
-    m = m + z
-print (m[::-1])
+s = str(input())
+t = (s[::-1])
+l = t.replace('A', 't')
+k = l.replace('T', 'a')
+m = k.replace('G', 'c')
+n = m.replace('C', 'g')
+print(n.upper())
